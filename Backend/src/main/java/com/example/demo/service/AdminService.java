2ee391007e4c313ddee80b0d99f3239812f03452
Kiran Pandiyan <150167333+kiranpandiyan04@users.noreply.github.com>
2024-08-09 09:28:30 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/example/demo/service/AdminService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Admin;
+import com.example.demo.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public List<Admin> getAllAdmins() {
+        return adminRepository.findAll();
+    }
+
+    public Admin getAdminById(Long id) {
+        return adminRepository.findById(id).orElse(null);
+    }
+
+    public Admin saveAdmin(Admin admin) {
+        return adminRepository.save(admin);
+    }
+
+    public void deleteAdmin(Long id) {
+        adminRepository.deleteById(id);
+    }
+
+    public List<Admin> getUser(String email,String pass) {
+        return adminRepository.findByEmailIdAndPassword(email,pass);
+    }
+
+    public Admin getByEmail(String email) {
+        return adminRepository.getByEmailId(email);
+    }
+}
